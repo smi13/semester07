c5a93a97fcc4d53426c8892fd9eb6f981cb30212
smi13 <fedka@ebanat.com>
2010-09-23 21:27:19 +0400

    Task 1.6 is working somehow

--- operating_systems/lab/lab1/taskmgr.sh
+++ operating_systems/lab/lab1/taskmgr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#1.6. [2] Напишите скрипт, управляющий списком текущих дел. Скрипт должен хранить
+# 1.6 [2] Напишите скрипт, управляющий списком текущих дел. Скрипт должен хранить
 #     список дел в файле .todo в текущем каталоге и предоставлять возможность
 #     добавления задачи, удаления задачи, отметки задачи как выполненной и
 #     просмотра всех задач. Режим работы задается параметром командной строки.
@@ -11,21 +11,35 @@
 #     командной строки. Справка печатается на экран, если среди параметров есть ключ
 #     «--help» или «-h», никаких других действий в этом случае выполнять не нужно.
 
+# TODO: Comment the code
 
 # Global help message variable
-HELP_MSG="Usage: ./taskmgr.sh [-g] [-a|-r|-d|-l]\nOptions: \n
+HELP_MSG="Usage: ./taskmgr.sh [-h] [-g] [-a|-r|-d|-l]\nOptions: \n
 	-g, --global \t Work with ~/.todo file, otherwise .todo file in current directory will be used \n\n
 	-a, --add \t Add new task \n
 	-r, --remove \t Remove task from the list \n
 	-d, --done \t Mark certain task as 'done' \n
-	-l, --list \t List present tasks \n
-	-h, --help \t Display this message \n
-  "
+	-l, --list \t Display present tasks \n
+	-h, --help \t Display this message \n"
 
-# Store script arguments in variable
 args=("$@")
 
 declare file_name
+declare line_count
+declare -a file_array
+
+function readFile()
+{
+   let count=0
+   
+   while read LINE; do
+
+      file_array[$count]=$LINE
+      ((count++))
+   done <$file_name
+
+   line_count=${#file_array[@]} 
+}
 
 # Find argument in array function
 function _findArg()
@@ -59,6 +73,7 @@ function findParameter()
    fi
 }
 
+# Check which file should be modified
 function determineFile()
 {
    findParameter -g --global
@@ -73,36 +88,144 @@ function determineFile()
    if [ ! -e $file_name ]; then
       touch $file_name
    fi
+
+   readFile
 }
 
 function addTask()
 {
-	echo "fuck"
+   local task_message
+   echo -e "Write task message:"
+   read task_message
+
+   file_array[$line_count]=$task_message
+   ((line_count++))
+}
+
+# Tests whether *entire string* is numerical.
+# In other words, tests for integer variable.
+function isDigit()    
+{               
+  [ $# -eq 1 ] || return 0
+
+  case $1 in
+    *[!0-9]*|"") return 0;;
+              *) return 1;;
+  esac
 }
 
 function deleteTask()
 {
-	echo "ss"
+   listTasks
+   echo -e "What task will be removed? (number)"
+   read number
+
+   isDigit $number
+
+   if [ $? -eq 0 ]; then
+      echo "Invalid number"
+      return
+   else
+     if [ $number -gt $line_count ] || [ $number -eq 0 ]; then
+        echo "Invalid number"    
+        return	
+     fi
+
+     ((number--))
+     unset file_array[$number]
+   fi
 }
 
 function markTaskDone()
 {
-	echo "yy"
+   listTasks
+   echo -e "What task is done? (number)"
+   read number
+
+   isDigit $number
+   
+   if [ $? -eq 0 ]; then
+      echo "Invalid number"
+      return
+   else
+     if [ $number -gt $line_count ] || [ $number -eq 0 ]; then
+        echo "Invalid number"    
+        return	
+     fi
+
+     ((number--))
+
+     if [[ ${file_array[$number]} =~ done ]]; then
+        echo "Task #$((number+1)) has been already marked as done"
+	return
+     fi
+   fi
+
+   file_array[$number]+=" (done)"
 }
 
 function listTasks()
 {
-	echo "aa"
+   for ((i=0; i<$line_count; i++)); do
+      if [ $# -eq 0 ]; then
+        echo $((i+1))")" ${file_array[$i]}	   
+      else
+        if [ -z "${file_array[$i]}" ]; then      
+           continue
+        else
+           echo ${file_array[$i]}	   
+        fi
+      fi
+   done
 }
 
 function processArguments()
 {
    determineFile
+
+   local command
+
+   #TODO: check -g order
+   if [ -z ${args[1]} ]; then
+      command=${args[0]}
+   else
+      command=${args[1]}
+   fi
+
+   case "$command" in
+      -a|--add)
+         addTask
+         ;;
+      -l|--list)
+         listTasks
+	 ;;
+      -r|--remove)
+      	 deleteTask
+	 ;;
+      -d|--done)
+         markTaskDone
+	 ;;
+       *)
+         echo -e "Invalid arguments\n"$HELP_MSG
+	 ;;       
+   esac      
 }
 
 # Entry point for the script
 function work()
 {
+   if [ ${#args[@]} -eq 0 ]; then
+      echo -e "No argument provided\n"$HELP_MSG
+      return
+   fi
+
+   if [ ${#args[@]} -eq 1 ]; then
+      if [ ${args[0]} == "-g" ] || [ ${args[0]} == "--global" ]; then
+         echo -e "No command provided\n"$HELP_MSG
+	 return
+      fi
+   fi
+  
    # If help argument provided, nothing more is required
    findParameter -h --help
    return_val=$?
@@ -112,6 +235,12 @@ function work()
    else
       # If not, going to process another arguments
       processArguments
+   fi
+
+   if [ -z "$file_name" ]; then
+      return
+   else
+      listTasks "fake_param" > $file_name
    fi
 }
 
